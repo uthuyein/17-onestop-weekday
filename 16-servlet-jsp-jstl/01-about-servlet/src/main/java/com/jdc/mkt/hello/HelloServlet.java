bb65dd665af6e35a99405e9045ac9265e1d33172
MKT <MKT@macbookpro>
2026-06-29 11:50:38 +0630

    update with servlet

---
+++ 16-servlet-jsp-jstl/01-about-servlet/src/main/java/com/jdc/mkt/hello/HelloServlet.java
@@ -0,0 +1,21 @@
+package com.jdc.mkt.hello;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter()
+		.append("<h1> Hello Servlet </h1>")
+		.append("<p>this is about servlet from servlet file </p>"); 
+	}
+}
